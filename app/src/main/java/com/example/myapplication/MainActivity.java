c0bb119d61886e37303c1f9d061808173f0953b1
ignis17 <joel.turbi@student.mec.cuny.edu>
2019-05-08 23:01:47 -0400

    New Changes to login - Video background

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 class LoginManager {
     /** Are the given user name and password known to the system? */
     public static boolean isKnownUser(String name, String psswd) {
-        if(name.equals("joelt88") && psswd.equals("password")) {
+        if(name.equals("test") && psswd.equals("passwd")) {
             return true;
         }
         return false;
@@ -23,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView videoview = findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.video3);
+        videoview.setVideoURI(uri);
+        videoview.start();
     }
     public void validateUser(View view){
         Intent intent = new Intent(this, WelcomeActivity.class);
@@ -30,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         EditText passw =  findViewById(R.id.editTextPsswd);
         String username = name.getText().toString();
         String password = passw.getText().toString();
-
         if(LoginManager.isKnownUser(username, password)){
             intent.putExtra("user",username);
             startActivity(intent);
@@ -39,4 +44,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Wrong Credentials. Try Again.",Toast.LENGTH_LONG).show();
         }
     }
+
 }
