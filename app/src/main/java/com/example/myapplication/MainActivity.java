9979ccc3c5fbcced5442fe5afcfbb71375c64f5a
ignis17 <joel.turbi@student.mec.cuny.edu>
2019-05-13 17:26:12 -0400

    New changes

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,9 +5,14 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.widget.VideoView;
+
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 
 class LoginManager {
     /** Are the given user name and password known to the system? */
@@ -19,8 +24,9 @@ class LoginManager {
     }
 }
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+    SignInButton signInButton;
+    Button signOutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
